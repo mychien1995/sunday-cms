0154394b984da55273380987e3e20b7f21bed6c8
chien.tran <chientm@smartosc.com>
2020-11-30 18:18:10 +0700

    Rendering Area Editor

--- Sunday.CMS.FrontEnd/sunday-cms/src/app/components/contents/shared/fields-renderer/renderers/rendering-area-renderer.component.ts
+++ Sunday.CMS.FrontEnd/sunday-cms/src/app/components/contents/shared/fields-renderer/renderers/rendering-area-renderer.component.ts
@@ -1,13 +1,23 @@
 import { OnInit, Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ContentField, TemplateField } from '@models/index';
+import {
+  ContentField,
+  Rendering,
+  RenderingValue,
+  TemplateField,
+} from '@models/index';
 import { RenderingSelectorDialogComponent } from './partials/rendering-selector-dialog.component';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { RenderingService } from '@services/index';
 
 @Component({
   selector: 'app-rendering-area-renderer',
   templateUrl: './rendering-area-renderer.component.html',
+  styleUrls: ['./rendering-area-renderer.component.scss'],
 })
 export class RenderingAreaRenderComponent implements OnInit {
+  renderings: RenderingValue[] = [];
+  renderingLookup: Rendering[] = [];
   innerField: ContentField;
   @Input()
   get field(): ContentField {
@@ -15,6 +25,11 @@ export class RenderingAreaRenderComponent implements OnInit {
   }
   set field(val: ContentField) {
     this.innerField = val;
+    if (val.value) {
+      this.renderings = val.value.Renderings;
+    } else {
+      this.renderings = [];
+    }
     this.fieldChange.emit(val);
   }
   @Output()
@@ -23,7 +38,10 @@ export class RenderingAreaRenderComponent implements OnInit {
   @Input() websiteId: string;
   @Input() contentId: string;
 
-  constructor(private dialogService: MatDialog) {}
+  constructor(
+    private dialogService: MatDialog,
+    private renderingService: RenderingService
+  ) {}
 
   openDialog() {
     const ref = this.dialogService.open(RenderingSelectorDialogComponent, {
@@ -31,13 +49,30 @@ export class RenderingAreaRenderComponent implements OnInit {
     });
     ref.componentInstance.load(
       (renderingVal) => {
-        console.log(renderingVal);
-        this.innerField.value = renderingVal;
+        this.renderings.push(renderingVal);
+        this.innerField.value = {
+          Renderings: this.renderings,
+        }
+        this.fieldChange.emit(this.innerField);
       },
       this.websiteId,
       this.contentId
     );
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.renderingService
+      .getRenderings({
+        IsPageRendering: false,
+        WebsiteId: this.websiteId,
+        PageSize: 1000,
+      })
+      .subscribe((res) => {
+        this.renderingLookup = res.List;
+      });
+  }
+
+  getRenderingName(id: string): string {
+    return this.renderingLookup.find((r) => r.Id === id)?.RenderingName;
+  }
 }
