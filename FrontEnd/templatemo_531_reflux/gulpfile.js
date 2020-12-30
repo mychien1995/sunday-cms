8a97662ed24b4feff9404d7460a39cedd07b06bb
chien.tran <mychien1995@gmail.com>
2020-12-30 22:23:53 +0700

    Styling

---
+++ FrontEnd/templatemo_531_reflux/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var minify = require('gulp-minify');
+
+const cssFiles = [
+	'vendor/**/*.css',
+	'assets/css/fontawesome.css',
+	'assets/css/owl.css',
+	'assets/css/templatemo-style.css',
+	'assets/css/custom.css'
+];
+// create task
+gulp.task('css', function(){
+    return gulp.src(cssFiles)
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('js', function(){
+    gulp.src(['assets/js/*.js'])
+        .pipe(minify())
+        .pipe(concat('site.min.js'))
+        .pipe(gulp.dest('dist/js'));
+    return gulp.src(['vendor/**/*.js'])
+        .pipe(minify())
+        .pipe(concat('vendor.min.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('assets', function(){
+	gulp.src(['assets/images/*']).pipe(gulp.dest('dist/images'));
+	return gulp.src(['assets/fonts/*']).pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('default', gulp.series('css', 'js', 'assets'));
